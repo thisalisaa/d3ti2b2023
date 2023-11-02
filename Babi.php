12706aaaed46013cbef3f1c211ec660ea4f01cc3
Hery1809 <heryslt09@gmail.com>
2023-11-02 16:19:50 +0700

    Babi

---
+++ Babi.php
@@ -0,0 +1,11 @@
+<?php 
+
+class Babi implements Hewan {
+
+    public function berkembangBiak(): void{
+        echo "Babi berkembang biak dengan Beranak";
+    }
+    public function berkomunikasi(): void{
+        echo "Babi berkomunikasi melalui Dengkuran";
+    }
+}
