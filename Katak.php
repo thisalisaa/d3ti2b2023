9cc07724782e6e4ad713961811e6cb551a04b142
ayusantika28 <ayusantika2802@gmail.com>
2023-11-02 15:46:13 +0700

    Katak

---
+++ Katak.php
@@ -0,0 +1,13 @@
+<?php
+
+class Katak Implements  Hewan{
+
+    public function berkembangBiak(){
+        echo " Katak berkembangbiak dengan cara bertelur";
+    }
+    public function berkomunikasi(){
+        echo "Katak berkomunikasi dengan cara bersuara";
+    }
+}
+
+?>
